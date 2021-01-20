8837135d32584afd7a155241f87c013479e9b66f
Mark McQuillen <markdmcq@hotmail.com>
2021-01-20 10:47:46 -0500

    request joke on click

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -1,12 +1,27 @@
 (ns app.hello
+  (:require-macros [cljs.core.async.macros :refer [go]])
+  (:require [cljs-http.client :as http]
+            [cljs.core.async :refer [<!]])
   (:require [reagent.core :as r]))
+
+(defn api-call []
+  (go
+    (let [api-response (<! (http/get "https://official-joke-api.appspot.com/random_joke" {:with-credentials? false}))]
+        (println "api-response:" api-response)
+        (println "body:" (:body api-response))
+        (println "setup:" (get-in api-response [:body :setup]))
+        (println "punchline:" (get-in api-response [:body :punchline]))
+
+        ; (get-in api-response [:body :setup])
+
+        )) nil )
 
 (defn click-counter [click-count]
   [:div
    "The atom " [:code "click-count"] " has value: "
    @click-count ". "
    [:input {:type "button" :value "Click me!"
-            :on-click #(swap! click-count inc)}]])
+            :on-click api-call}]])
 
 (def click-count (r/atom 0))
 
@@ -14,4 +29,6 @@
   [:<>
    [:p "Hello, my-shadow-app is running!"]
    [:p "Here's an example of using a component with state:"]
-   [click-counter click-count]])
+   [click-counter click-count]
+   [api-call]
+])
