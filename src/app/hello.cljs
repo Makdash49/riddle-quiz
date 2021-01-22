0a15ea1f81f14f3df968de8d8826c5451d4a1912
Mark McQuillen <markdmcq@hotmail.com>
2021-01-22 17:03:16 -0500

    its working

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -8,7 +8,7 @@
 (defn api-call [joke-map]
   (reset! joke-map {})
   (loop [x 0]
-    (println "recur!!!!!")
+    ; (println "recur!!!!!")
     (when (< x 4)
     (go
       (let [api-response (<! (http/get "https://official-joke-api.appspot.com/random_joke" {:with-credentials? false}))]
@@ -25,12 +25,12 @@
     (when (> (count items) 3)
     [:ul
     (for [item items]
-      ^{:key item} [:li (nth letters (.indexOf items item)) " " (:punchline item)])])))
+      ^{:key item} [:li "("(nth letters (.indexOf items item)) ")  " (:punchline item)])])))
 
 (defn letter-display [joke-map]
   [:p "You selected letter: "
   (when (:letter @joke-map)
-  (str/upper-case (:letter @joke-map)))])
+  (:letter @joke-map))])
 
 (defn joke-display [joke-map]
   [:div
@@ -42,12 +42,30 @@
 
 (def joke-map (r/atom {}))
 
-  (defn handler [e] (println "This is e: " (.-key e))
-   (swap! joke-map assoc :letter (.-key e)))
+  (defn handler [e]
+    ; (println "This is e: " (.-key e))
+   (swap! joke-map assoc :letter  (str/upper-case (.-key e)  )))
+
+(defn answer [joke-map]
+  (println "for answer - joke-map: " @joke-map)
+  (let [setup-id (get-in @joke-map [:display-setup-joke :id])
+        selected-letter (@joke-map :letter)
+        letters-to-nums {"A" 0 "B" 1 "C" 2 "D" 3}
+        joke-index (letters-to-nums (@joke-map :letter))
+        selection-id (get-in @joke-map [:shuffled-jokes joke-index :id])]
+        (println "set-up id: " setup-id)
+        (println "selected-letter: " selected-letter)
+        (println "joke-index:" joke-index)
+        (println "selection-id: " selection-id)
+        (when (some? selected-letter)
+          (if (= setup-id selection-id)
+          "YOU GOT IT RIGHT!!!"
+          "WRONG!!!"))))
 
 (defn hello []
     (js/document.addEventListener "keypress" handler)
   [:<>
     [:p "RIDDLE QUIZ!"]
     [joke-display joke-map]
-    [letter-display joke-map]])
+    [letter-display joke-map]
+    [answer joke-map]])
