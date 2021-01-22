a9a6283ec28f1064581d6d3818ac49ce98235416
Mark McQuillen <markdmcq@hotmail.com>
2021-01-22 13:49:43 -0500

    Display letter without shuffling punchlines, one shuffle needed

--- src/app/hello.cljs
+++ src/app/hello.cljs
@@ -14,14 +14,14 @@
       (recur (inc x)))))
 
 (defn first-joke [joke-map]
-  (let [map-keys   (map key joke-map)
+  (let [map-keys   (map key (joke-map :jokes))
         first-key  (first map-keys)
-        first-joke (joke-map first-key)
+        first-joke (get-in joke-map [:jokes first-key])
         ]
-  (prn "joke-map:" joke-map)
-  (prn "map-keys: " map-keys)
-  (prn "first-key:" first-key)
-  (println "first-joke:" first-joke)
+  (prn "*joke-map:" joke-map)
+  (prn "*map-keys: " map-keys)
+  (prn "*first-key:" first-key)
+  (println "*first-joke:" first-joke)
   first-joke
   ))
 
@@ -31,14 +31,16 @@
       (for [item items]
       ^{:key item} [:li (:punchline item)])]))
 
+(defn letter-display [joke-map]
+  [:p "You selected letter: " (:letter @joke-map)])
+
 (defn joke-display [joke-map]
   [:div
     [:input {:type "button" :value "Click me!"
             :on-click #(api-call joke-map)}]
     [:p (:setup (first-joke @joke-map))]
-    [:p "Is this your letter? " (:letter @joke-map)]
-       [lister (shuffle (map val (@joke-map :jokes)))]
-      ])
+    (println "(map val (@joke-map :jokes)):" (map val (@joke-map :jokes)))
+       [lister (map val (@joke-map :jokes))]])
 
 (def joke-map (r/atom {}))
 
@@ -48,5 +50,6 @@
 (defn hello []
     (js/document.addEventListener "keypress" handler)
   [:<>
-    [:p "RANDOM JOKES!"]
-    [joke-display joke-map]])
+    [:p "RIDDLE QUIZ!"]
+    [joke-display joke-map]
+    [letter-display joke-map]])
